8ce023385bba23d49a2fa5f993eca8d2ff858043
Rodrigo <rodrigo.ldovr@gmail.com>
2021-11-15 15:49:42 -0300

    posto localidade para resolver problemas de caracteres

--- adivinhação.c
+++ adivinhação.c
@@ -1,7 +1,12 @@
 
 #include <stdio.h>
+#include <locale.h>
 
 int main(){
+	
+	setlocale(LC_ALL, "Portuguese");
+
+	//imprime o cabeçalho do jogo:
 	printf("*******************************************\n");
 	printf("* Bem-vindo ao nosso jogo de adivinhação! *\n");
 	printf("*******************************************\n");
