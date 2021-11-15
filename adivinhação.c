fd0b693eadf8556f3ffba871048b4b0b1aee1c18
Rodrigo <rodrigo.ldovr@gmail.com>
2021-11-15 16:19:21 -0300

    retirado setlocale, problema acentuação resolvida com chcp 65001

--- adivinhação.c
+++ adivinhação.c
@@ -1,13 +1,14 @@
 
 #include <stdio.h>
-#include <locale.h>
 
 int main(){
 	
-	setlocale(LC_ALL, "Portuguese");
-
 	//imprime o cabeçalho do jogo:
 	printf("*******************************************\n");
 	printf("* Bem-vindo ao nosso jogo de adivinhação! *\n");
 	printf("*******************************************\n");
+
+	int numerosecreto = 42;
+
+	printf("O número secreto é %d! Não conte para ninguém!", numerosecreto);
 }
