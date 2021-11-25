1e3f5d489895bd717a1a3d245106300507a8530e
Rodrigo <rodrigo.ldovr@gmail.com>
2021-11-25 09:33:05 -0300

    Posto o loop na pergunta do jogo (repete a pergunta 3 x)

--- adivinhação.c
+++ adivinhação.c
@@ -16,8 +16,33 @@ int main(){
 
 	int chute;
 
-	printf("Tu sabe qual é o número que o jogo pensou?");
-	scanf("%d", &chute);
-	printf("Então tu acha que o número que jogo tem guardado é %d?\n", chute);
-	system("pause");
+	for(int i = 1; i <=3; i++){
+		printf("Tu sabe qual é o número que o jogo pensou?");
+		scanf("%d", &chute);
+		printf("Então tu acha que o número que jogo tem guardado é %d?\n", chute);
+		system("pause");
+		printf("Vou ver ser é verdade...\n");
+		sleep(3);
+
+		//Verificação se o jogador adivinhou o número secreto:
+
+		if(chute == numerosecreto){
+			printf("Parabéns!! Tu acertou o número que o jogo pensou!\n");
+			printf("Quer jogar mais uma vez?\n");
+			system("pause");
+		}
+		else{
+			printf("Hmmm... Você errou o número que jogo pensou...\n");
+			printf("Mas não desanime. Tente de novo!\n");
+
+			if (chute > numerosecreto){
+				printf("Dica: o seu chute foi maior que o número pensado pelo jogo.\n");
+			}
+
+			if (chute < numerosecreto){
+				printf("Dica: o seu chute foi menor que o número pensado pelo jogo.\n");
+			}
+			system("pause");
+		}
+	}
 }
