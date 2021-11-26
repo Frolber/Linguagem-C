17deef9d88459d7b50fe279ea2048cb423c07813
Rodrigo <rodrigo.ldovr@gmail.com>
2021-11-26 09:32:10 -0300

    ajustado os nomes das variáveis e escrita do if

--- adivinhação.c
+++ adivinhação.c
@@ -3,6 +3,8 @@
 #include <locale.h>
 #include <unistd.h>
 
+#define NUMEROS_DE_TENTATIVAS 5
+
 int main(){
 
 	setlocale(LC_ALL, "");
@@ -17,34 +19,42 @@ int main(){
 	int chute;
 
 	//Esse for é loop para que o jogador tenha só três chances para acertar o número secreto:
-	for(int i = 1; i <=3; i++){
-		printf("Sua %d tentativa de 3 no momento\n", i);
+	for(int i = 1; i <=NUMEROS_DE_TENTATIVAS; i++){
+		printf("Sua %d tentativa de %d no momento\n", i, NUMEROS_DE_TENTATIVAS);
 		printf("Tu sabe qual é o número que o jogo pensou?");
 		scanf("%d", &chute);
 		printf("Então tu acha que o número que jogo tem guardado é %d?\n", chute);
 		system("pause");
 		printf("Vou ver ser é verdade...\n");
-		sleep(3);
+		sleep(2);
 
 		//Verificação se o jogador adivinhou o número secreto:
 
-		if(chute == numerosecreto){
+		int acertou = (chute == numerosecreto);
+
+		if(acertou){
 			printf("Parabéns!! Tu acertou o número que o jogo pensou!\n");
 			printf("Quer jogar mais uma vez?\n");
-			system("pause");
+			break;
 		}
 		else{
 			printf("Hmmm... Você errou o número que jogo pensou...\n");
 			printf("Mas não desanime. Tente de novo!\n");
 
-			if (chute > numerosecreto){
+			int chutemaior = (chute > numerosecreto);
+
+			if (chutemaior){
 				printf("Dica: o seu chute foi maior que o número pensado pelo jogo.\n");
 			}
 
-			if (chute < numerosecreto){
+			int chutemenor = (chute < numerosecreto);
+
+			if (chutemenor){
 				printf("Dica: o seu chute foi menor que o número pensado pelo jogo.\n");
-			}
-			system("pause");
+			}		
 		}
 	}
+	printf("Fim de jogo!!\n");
+	system("pause");
 }
+
